616b32bd8adf915b04cedd95bda205a9c54f2d7f
Uncensored-Developer <osezele.o@gmail.com>
2020-05-29 11:05:13 +0000

    Display fetched employees in MainActivity with RecyclerView

--- app/src/main/java/com/globomed/learn/MainActivity.kt
+++ app/src/main/java/com/globomed/learn/MainActivity.kt
@@ -1,15 +1,27 @@
 package com.globomed.learn
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private val employeeListAdapter = EmployeeListAdapter(this)
+    private lateinit var databaseHelper: DatabaseHelper
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        databaseHelper = DatabaseHelper(this)
+
+        recyclerView.adapter = employeeListAdapter
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        employeeListAdapter.setEmployees(DataManager.fetchAllEmployees(databaseHelper))
 
         fab.setOnClickListener {
             val addEmployee = Intent(this, AddEmployeeActivity::class.java)
@@ -19,5 +31,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
+
+        if (resultCode == Activity.RESULT_OK) employeeListAdapter.setEmployees(DataManager.fetchAllEmployees(databaseHelper))
     }
 }
